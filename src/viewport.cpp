4ee86dd4bb8d3ec8cf61319ccc7902f2b1ad8f38
cookieli <1942374438@qq.com>
2020-01-05 21:18:26 +0800

    finish transformation

--- src/viewport.cpp
+++ src/viewport.cpp
@@ -11,7 +11,17 @@ void ViewportImp::set_viewbox( float centerX, float centerY, float vspan ) {
   // arguments are defined as normalized SVG canvas coordinates.
   this->centerX = centerX;
   this->centerY = centerY;
-  this->vspan = vspan; 
+  this->vspan = vspan;
+  Matrix3x3 translation1 = Matrix3x3::identity();
+  translation1(0, 2) -= centerX;
+  translation1(1, 2) -= centerY;
+  Matrix3x3 scale = Matrix3x3::identity();
+  scale(0, 0)     /= 2*vspan;
+  scale(1, 1)     /= 2*vspan;
+  Matrix3x3 translation2 = Matrix3x3::identity();
+  translation2(0, 2) += 0.5;
+  translation2(1, 2) += 0.5;
+  set_svg_2_norm(translation2*scale*translation1);
 
 }
 
